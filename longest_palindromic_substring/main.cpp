8bf2d81848a4bdb5429dde5916da69d223deb075
Nicolas <n.rioumarc@gmail.com>
2026-02-09 12:06:52 +0100

    longest_palindromic_substring

--- longest_palindromic_substring/main.cpp
+++ longest_palindromic_substring/main.cpp
@@ -4,55 +4,55 @@
 using namespace std;
 
 class Solution {
-  private:
-    bool isPalindrome(string s) {
-        for (string::size_type i = 0; i < s.size() / 2; i++) {
-            if (s[i] != s[s.size() - 1 - i]) {
-                return 0;
-            }
-        }
-        return 1;
+private:
+  bool isPalindrome(string s) {
+    for (string::size_type i = 0; i < s.size() / 2; i++) {
+      if (s[i] != s[s.size() - 1 - i]) {
+        return 0;
+      }
     }
+    return 1;
+  }
 
-  public:
-    string longestPalindrome(string s) {
+public:
+  string longestPalindrome(string s) {
 
-        string current, longest;
+    string current, longest;
 
-        if (s.size() <= 1) {
-            return s;
+    if (s.size() <= 1) {
+      return s;
+    }
+    longest = s[0];
+
+    if (s.size() == 2) {
+      if (isPalindrome(s)) {
+        return s;
+      }
+      return longest;
+    }
+    current = std::string(s.begin(), s.end());
+    if (isPalindrome(current))
+      return current;
+
+    for (string::size_type end = s.size(); end > 0; end--) {
+      for (string::size_type start = 0; start < s.size(); start++) {
+
+        string current = string(s.begin() + start, s.begin() + start + end);
+
+        if (isPalindrome(current)) {
+          return current;
         }
-        longest = s[0];
-
-        if (s.size() == 2) {
-            if (isPalindrome(s)) {
-                return s;
-            }
-            return longest;
-        }
-
-        for (string::size_type i = 0; i < s.size(); i++) {
-            current.push_back(s[i]);
-            for (string::size_type j = i + 1; j < s.size(); j++) {
-
-                current.push_back(s[j]);
-
-                if (isPalindrome(current)) {
-                    if (current.size() > longest.size())
-                        longest = current;
-                }
-            }
-            current = "";
-        }
-        return longest;
-    };
+      }
+    }
+    return "";
+  };
 };
 
 int main(void) {
 
-    Solution sol;
+  Solution sol;
 
-    cout << "(bab)" << sol.longestPalindrome("babad") << endl;
+  cout << "(bab)" << sol.longestPalindrome("ttbabad") << endl;
 
-    return 0;
+  return 0;
 }
